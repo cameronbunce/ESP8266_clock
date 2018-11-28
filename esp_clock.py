c234b0b33eab70b08b044160d1c3ba11af034bad
Cameron <charliesinfiniteride@gmail.com>
2018-11-28 06:57:30 -0500

    Update esp_clock.py

--- esp_clock.py
+++ esp_clock.py
@@ -4,30 +4,15 @@ import time
 import ntptime
 import bitbangio as io
 import adafruit_ht16k33.segments
-# import adafruit_ssd1306
-
 
 ssid = 'SixTwentyFour'
 password = 'cameronanddominique'
 
 i2c = io.I2C(board.SCL, board.SDA)
-# oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 display = adafruit_ht16k33.segments.Seg14x4(i2c)
-
 display.fill(0)
 
-# oled.fill(0)
-# oled.text('Circuit Python', 0, 0)
-# oled.text('NTP clock', 0, 10)
-# oled.show()
-# time.sleep(0.25)
-
 sta_if = network.WLAN(network.STA_IF)
-
-# oled.fill(0)
-# oled.text('Connecting to', 0, 0)
-# oled.text(ssid, 0, 10)
-# oled.show()
 
 if not sta_if.isconnected():
     sta_if.active(True)
@@ -35,14 +20,9 @@ if not sta_if.isconnected():
     while not sta_if.isconnected():
         pass
         
-# oled.text("IP: " + sta_if.ifconfig()[0], 0, 20)
-# oled.show()
-# time.sleep(0.25)
 
-TZ = 14400 # UTC to EDT
-# SECS = 43200 # 12-hour time
-# SECS = 86400 # 24-hour time
-
+#TZ = 14400 # UTC to EDT
+TZ = 18000 # UTC to EST
 
 t = None
 while not t:
@@ -59,21 +39,25 @@ while True:
     tm = t - mono_time + int(time.monotonic())
     now = time.localtime(tm-TZ)
     hr = now.tm_hour % 12 # 12-hour time for humans
-    
+    zero = 0
     if hr == 0:
         hr = 12 # also for humans
+        # to-do, check NTP again and reset ms offsets to keep the planes in the air without a reboot
     
     
 #    if (tm-TZ)%43200 < 6: # possibly obo, but not on the skip side, to reset the display to not show the 1 in the tens column when the clock goes from 12 to 1
 #        display.fill(0)
-    if int(hr/10) == 0:
-        display[0] = ''
+    if int(hr/10) == 0 and zero == 0:
+        display.fill(0)
+        zero += 1
+    elif int(hr/10) == 0:
+        pass
     else:
         display[0] = '1' # give me a break, its 12-hour time
-        
+        zero = 0
     display[1] = str(int(hr % 10))
     display[2] = str(int(now.tm_min/10))
     display[3] = str(int(now.tm_min % 10))
     display.show()
-    time.sleep(5) # this is the counter of "how far behind can we wait to flip the minute"? 5 seconds, impacts our tens of hours reset
+    time.sleep(10) # this is the counter of "how far behind can we wait to flip the minute"? 5 seconds, impacts our tens of hours reset
     
